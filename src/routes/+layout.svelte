2e7062f524205f79a3038f9cc23e45933d0e5332
tapeds <frrllmatthew@gmail.com>
2024-10-30 22:16:50 +0700

    chore: install tanstack-query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+import { browser } from "$app/environment";
+import "../app.css";
+
+let { children } = $props();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      enabled: browser,
+    },
+  },
+});
 </script>
 
+<QueryClientProvider client={queryClient}>
 {@render children()}
+</QueryClientProvider>
